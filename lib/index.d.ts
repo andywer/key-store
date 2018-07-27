c3786e642c5864c97222b5af4c515260eda71dd6
Andy Wermke <andy.dev@next-step-software.com>
2018-07-27 18:35:12 +0200

    Include the declaration file in git

---
+++ lib/index.d.ts
@@ -0,0 +1,23 @@
+export interface KeyStore<PrivateKeyData, PublicKeyData> {
+    getKeyIDs(): string[];
+    getPublicKeyData(keyID: string): PublicKeyData;
+    getPrivateKeyData(keyID: string, password: string): PrivateKeyData;
+    saveKey(keyID: string, password: string, privateData: PrivateKeyData, publicData?: PublicKeyData): Promise<void>;
+    removeKey(keyID: string): Promise<void>;
+}
+interface KeyMetadata {
+    initVector: string;
+    iterations: number;
+}
+export interface KeysData<PublicKeyData> {
+    [keyID: string]: {
+        metadata: KeyMetadata;
+        public: PublicKeyData;
+        private: string;
+    };
+}
+export declare type SaveKeys<PublicKeyData> = (data: KeysData<PublicKeyData>) => Promise<void> | void;
+export declare function createStore<PrivateKeyData, PublicKeyData = {}>(save: SaveKeys<PublicKeyData>, initialKeys?: KeysData<PublicKeyData>, options?: {
+    iterations?: number;
+}): KeyStore<PrivateKeyData, PublicKeyData>;
+export {};
